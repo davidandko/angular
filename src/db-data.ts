212a83aba5a295b5f835a76dc5fac5be9db5503d
davidandko <davidandko.ai@gmail.com>
2024-01-11 20:02:02 +0100

    od cas

---
+++ src/db-data.ts
@@ -0,0 +1,40 @@
+
+
+export const DRIVERS: any = [
+
+    //undefined
+
+    {
+        id: 1,
+        name: "David Arsovski",
+        iconUrl: 'https://img.asmedia.epimg.net/resizer/3kixINBSED2oBv6JOUpyTjKqB6Q=/736x414/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/NAI2CC5JMYXG5552LVJGMGG5GA.jpg',
+        description: "Mnogu brz",
+        category: 'INTERMEDIATE',
+        lessonsCount: 410
+    },
+    {
+        id: 2,
+        name: "Kristina Kalamukoska",
+        iconUrl: 'https://img.asmedia.epimg.net/resizer/db5wseQ4_uInNJIhPaH_2ZIYMy4=/736x414/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/7GRTQT6C3I5EUE5ILE5U5PBRZU.jpg',
+        description: "Ekstra brza domakinka devojka",
+        category: 'PRO',
+        lessonsCount: 310
+    },
+    {
+        id: 3,
+        name: "Mia Darkovska",
+        iconUrl: 'https://img.asmedia.epimg.net/resizer/p97vj2fydECZyPHzavBKxRwW4QU=/736x414/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/FFZK2SQ3JJFCEVEEC7VDLHH3K4.jpg',
+        description: "Najspora na svetot posledna",
+        category: 'MAX',
+        lessonsCount: 210
+    },
+    {
+        id: 3,
+        name: "Mizimak",
+        iconUrl: 'https://img.asmedia.epimg.net/resizer/p97vj2fydECZyPHzavBKxRwW4QU=/736x414/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/FFZK2SQ3JJFCEVEEC7VDLHH3K4.jpg',
+        description: "Najspora na svetot posledna",
+        category: 'PRO',
+        lessonsCount: 210
+    }
+
+];
